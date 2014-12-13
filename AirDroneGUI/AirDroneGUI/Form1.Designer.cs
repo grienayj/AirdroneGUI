90fec983397ae6817e5ab89482419f1fb166fba7
grienayj <grienayj@me.com>
2014-12-13 17:09:39 +0100

    v0.2

---
+++ AirDroneGUI/AirDroneGUI/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace AirDroneGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Trottle_progressBar = new System.Windows.Forms.ProgressBar();
+            this.Trottle_label = new System.Windows.Forms.Label();
+            this.Connect_button = new System.Windows.Forms.Button();
+            this.label_rval = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Trottle_progressBar
+            // 
+            this.Trottle_progressBar.Location = new System.Drawing.Point(12, 64);
+            this.Trottle_progressBar.Name = "Trottle_progressBar";
+            this.Trottle_progressBar.Size = new System.Drawing.Size(248, 27);
+            this.Trottle_progressBar.Step = 5;
+            this.Trottle_progressBar.TabIndex = 0;
+            // 
+            // Trottle_label
+            // 
+            this.Trottle_label.AutoSize = true;
+            this.Trottle_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Trottle_label.Location = new System.Drawing.Point(121, 94);
+            this.Trottle_label.Name = "Trottle_label";
+            this.Trottle_label.Size = new System.Drawing.Size(24, 25);
+            this.Trottle_label.TabIndex = 1;
+            this.Trottle_label.Text = "0";
+            // 
+            // Connect_button
+            // 
+            this.Connect_button.Location = new System.Drawing.Point(12, 12);
+            this.Connect_button.Name = "Connect_button";
+            this.Connect_button.Size = new System.Drawing.Size(110, 32);
+            this.Connect_button.TabIndex = 2;
+            this.Connect_button.Text = "Connect";
+            this.Connect_button.UseVisualStyleBackColor = true;
+            this.Connect_button.Click += new System.EventHandler(this.Connect_button_Click);
+            // 
+            // label_rval
+            // 
+            this.label_rval.AutoSize = true;
+            this.label_rval.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_rval.Location = new System.Drawing.Point(190, 19);
+            this.label_rval.Name = "label_rval";
+            this.label_rval.Size = new System.Drawing.Size(24, 25);
+            this.label_rval.TabIndex = 3;
+            this.label_rval.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(279, 129);
+            this.Controls.Add(this.label_rval);
+            this.Controls.Add(this.Connect_button);
+            this.Controls.Add(this.Trottle_label);
+            this.Controls.Add(this.Trottle_progressBar);
+            this.Name = "Form1";
+            this.Text = "AirDroneGUI";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar Trottle_progressBar;
+        private System.Windows.Forms.Label Trottle_label;
+        private System.Windows.Forms.Button Connect_button;
+        private System.Windows.Forms.Label label_rval;
+    }
+}
+
---
+++ AirDroneGUI/AirDroneGUI/Form1.cs
@@ -0,0 +1,465 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace AirDroneGUI
+{
+    public partial class Form1 : Form
+    {
+
+        volatile List<String> keys = new List<String>();
+        Thread ComThread = null;
+        volatile Boolean connected = false;
+        SerialPort port;
+        volatile int motVal = 0;
+        volatile int Ctheta = 0;
+        volatile int Cphi = 0;
+        volatile int Cpsi = 0;
+
+        volatile int rval = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!keys.Contains(e.KeyCode.ToString()))
+            { keys.Add(e.KeyCode.ToString()); }
+            
+            if (keys.Contains("Z"))
+            {
+                motVal++;
+
+            }
+            else if (keys.Contains("S"))
+            {
+                motVal--;
+            }
+
+            if (motVal > 100)
+            { motVal = 100; }
+            if (motVal < 0)
+            { motVal = 0; }
+            /*
+            if (connected)
+            {
+                //byte[] b = new byte[3];
+                /*if (keys.Contains("z"))
+                {
+                    motVal++;
+
+                }
+                else if (keys.Contains("s"))
+                {
+                    motVal--;
+                }*
+
+                if (motVal > 100)
+                { motVal = 100; }
+                if (motVal < 0)
+                { motVal = 0; }
+                
+                b[0] = Convert.ToByte(0);
+                b[1] = Convert.ToByte(motVal);
+                b[2] = Convert.ToByte(motVal);
+                /*b[3] = Convert.ToByte(motVal);
+                b[4] = Convert.ToByte(motVal);
+                b[5] = Convert.ToByte(motVal);
+                b[6] = Convert.ToByte(motVal);
+                b[7] = Convert.ToByte(motVal);
+                b[8] = Convert.ToByte(motVal);*
+                port.Write(b, 0, 3);
+                //Thread.Sleep(10);*
+            }*/
+
+            Trottle_label.Text = motVal.ToString();
+            Trottle_progressBar.Value = motVal;
+
+            label_rval.Text = rval.ToString();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (keys.Contains(e.KeyCode.ToString()))
+            { keys.Remove(e.KeyCode.ToString()); }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (connected)
+            {
+                port.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+/*
+        private volatile int _trottleValue = 0;
+        public delegate int TrottleValueDelagate(int val);
+        public int trottleValue
+        {
+           // get
+          //  {
+               // TrottleValueDelagate del = new TrottleValueDelagate(GetTrottleValue);
+          //      if (!InvokeRequired)
+          //      {
+           //         return _trottleValue;
+           //         //return true;
+           //     }
+            //    else
+            //    {
+                   //return (int)Invoke(del, new int() { val });
+                   // return (int)Invoke(del, new int());
+            //    }
+          //  }
+            set
+            {
+                if (!InvokeRequired)
+                {
+                    // buttonConnect.Enabled = value;
+                    _trottleValue = value;
+                    Trottle_label.Text = value.ToString();
+                    Trottle_progressBar.Value = value;
+                }
+                else
+                {
+                    Invoke(new Action(() => trottleValue = value));
+                    //Invoke(new Action<int>(UpdateProgress), percentComplete);
+                }
+
+            }
+        }
+        private int GetTrottleValue(int b)
+        {
+            return _trottleValue;
+        }
+        */
+        private void Connect_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!connected)
+                {
+                    port = new SerialPort("COM6", 38400);
+                    port.Open();
+                    //while (!port.IsOpen) { }
+                    Connect_button.Text = "Disconnect";
+                    ///
+                    Thread.Sleep(100);
+                    ComThread = new System.Threading.Thread(() => ComThread_Task());
+                    ComThread.Start();
+                    connected = true;
+                }
+                else
+                {
+                    port.Close();
+                    ComThread.Abort();
+                    Connect_button.Text = "Connect";
+                    connected = false;
+                }
+            }
+            catch(System.IO.IOException err) { MessageBox.Show("connection error : "+err.Message); }
+        }
+
+        private void ComThread_Task()
+        {
+            try
+            {
+                while (connected)
+                {
+                    byte[] b = new byte[5];
+                    /*if (keys.Contains("z"))
+                    {
+                        motVal++;
+
+                    }
+                    else if (keys.Contains("s"))
+                    {
+                        motVal--;
+                    }*/
+
+                    if (motVal > 100)
+                    { motVal = 100; }
+                    if (motVal <= 0)
+                    { motVal = 1; }
+
+                    b[0] = Convert.ToByte(motVal);
+                    b[1] = Convert.ToByte(motVal);
+                    b[2] = Convert.ToByte(motVal);
+                    b[3] = Convert.ToByte(motVal);
+                    //b[4] = Convert.ToByte(motVal);
+                    /*b[5] = Convert.ToByte(motVal);
+                    b[6] = Convert.ToByte(motVal);
+                    b[7] = Convert.ToByte(motVal);
+                    b[8] = Convert.ToByte(motVal);*/
+                    port.Write(b, 0, 1);
+                    port.Write(b, 1, 1);
+                    port.Write(b, 2, 1);
+                    port.Write(b, 3, 1);
+                    //port.Read(b, 0, 1);
+
+                    rval = Convert.ToInt32(b[0]);
+
+                   // Thread.Sleep(10);
+                }
+            }
+            catch (System.IO.IOException err) { MessageBox.Show("send error : " + err.Message); }
+        }
+
+    }
+}
+/*
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+
+namespace AirDroneGUI
+{
+    public partial class Form1 : Form
+    {
+
+        volatile List<String> keys = new List<String>();
+        Thread ComThread = null;
+        volatile Boolean connected = false;
+        SerialPort port;
+        volatile int motVal = 0;
+
+        volatile byte labelText=6;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!keys.Contains(e.KeyCode.ToString()))
+            { keys.Add(e.KeyCode.ToString()); }
+            
+            if (keys.Contains("Z"))
+            {
+                motVal++;
+
+            }
+            else if (keys.Contains("S"))
+            {
+                motVal--;
+            }
+
+            if (motVal > 100)
+            { motVal = 100; }
+            if (motVal < 0)
+            { motVal = 0; }
+            /*
+            if (connected)
+            {
+                //byte[] b = new byte[3];
+                /*if (keys.Contains("z"))
+                {
+                    motVal++;
+
+                }
+                else if (keys.Contains("s"))
+                {
+                    motVal--;
+                }*
+
+                if (motVal > 100)
+                { motVal = 100; }
+                if (motVal < 0)
+                { motVal = 0; }
+                
+                b[0] = Convert.ToByte(0);
+                b[1] = Convert.ToByte(motVal);
+                b[2] = Convert.ToByte(motVal);
+                /*b[3] = Convert.ToByte(motVal);
+                b[4] = Convert.ToByte(motVal);
+                b[5] = Convert.ToByte(motVal);
+                b[6] = Convert.ToByte(motVal);
+                b[7] = Convert.ToByte(motVal);
+                b[8] = Convert.ToByte(motVal);*
+                port.Write(b, 0, 3);
+                //Thread.Sleep(10);*
+            }*
+
+            Trottle_label.Text = motVal.ToString();
+            Trottle_progressBar.Value = motVal;
+           // label1.Text = labelText.ToString();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (keys.Contains(e.KeyCode.ToString()))
+            { keys.Remove(e.KeyCode.ToString()); }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (connected)
+            {
+                port.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+/*
+        private volatile int _trottleValue = 0;
+        public delegate int TrottleValueDelagate(int val);
+        public int trottleValue
+        {
+           // get
+          //  {
+               // TrottleValueDelagate del = new TrottleValueDelagate(GetTrottleValue);
+          //      if (!InvokeRequired)
+          //      {
+           //         return _trottleValue;
+           //         //return true;
+           //     }
+            //    else
+            //    {
+                   //return (int)Invoke(del, new int() { val });
+                   // return (int)Invoke(del, new int());
+            //    }
+          //  }
+            set
+            {
+                if (!InvokeRequired)
+                {
+                    // buttonConnect.Enabled = value;
+                    _trottleValue = value;
+                    Trottle_label.Text = value.ToString();
+                    Trottle_progressBar.Value = value;
+                }
+                else
+                {
+                    Invoke(new Action(() => trottleValue = value));
+                    //Invoke(new Action<int>(UpdateProgress), percentComplete);
+                }
+
+            }
+        }
+        private int GetTrottleValue(int b)
+        {
+            return _trottleValue;
+        }
+        *
+        private void Connect_button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!connected)
+                {
+                    port = new SerialPort("COM4", 38400);
+                    port.Open();
+                    //while (!port.IsOpen) { }
+                    Connect_button.Text = "Disconnect";
+                    ///
+                    Thread.Sleep(100);
+                    ComThread = new System.Threading.Thread(() => ComThread_Task());
+                    ComThread.Start();
+                    connected = true;
+                }
+                else
+                {
+                    port.Close();
+                    ComThread.Abort();
+                    Connect_button.Text = "Connect";
+                    connected = false;
+                }
+            }
+            catch(System.IO.IOException err) { MessageBox.Show("connection error : "+err.Message); }
+        }
+
+        private void ComThread_Task()
+        {
+            try
+            {
+                while (connected)
+                {
+                    byte[] b = new byte[5];
+                    /*if (keys.Contains("z"))
+                    {
+                        motVal++;
+
+                    }
+                    else if (keys.Contains("s"))
+                    {
+                        motVal--;
+                    }*
+
+                    if (motVal > 100)
+                    { motVal = 100; }
+                    if (motVal < 0)
+                    { motVal = 0; }
+
+                    b[0] = Convert.ToByte(motVal);
+                    
+                    b[1] = Convert.ToByte(motVal);
+                    b[2] = Convert.ToByte(motVal);
+                    b[3] = Convert.ToByte(motVal);
+                    b[4] = Convert.ToByte(motVal);
+                    
+                    //b[5] = Convert.ToByte(crc(b,5));
+                    
+                    /*b[6] = Convert.ToByte(motVal);
+                    b[7] = Convert.ToByte(motVal);
+                    b[8] = Convert.ToByte(motVal);*
+                    port.Write(b, 0, 1);
+                    Thread.Sleep(10);
+                }
+            }
+            catch (System.IO.IOException err) { MessageBox.Show("send error : " + err.Message); }
+        }
+
+        private byte crc(byte[] data, int length)
+        {
+            byte result=0xff;
+            byte poly=0xcd;
+
+            for(int i=0;i<length;i++)
+            {
+                if(((result^data[i]) & 0x80)>0)
+                {
+                    result = (byte)((result << (int)1) ^ poly);
+                }else
+                {
+                    result = (byte)(result << (int)1);
+                }
+            }
+
+            labelText = result;
+
+            return result;
+        }
+
+    }
+}
+*/
